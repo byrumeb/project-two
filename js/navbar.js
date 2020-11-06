fec39f31f73491acd5178b9e903c01ba436e76d4
EB <byrumeb@gmail.com>
2020-11-05 23:46:02 -0500

    nav bar?

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+<div class="navbar">
+  <a class="active"   href="#index.html">Home</a>
+  <a href="#inside-page-one">News</a>
+  <a href="#inside-page-two">Gallery</a>
+  <a href="#inside-page-three">About</a>
+</div>
+
+
