4380494b9b4e7831076b226c1774eed74a6be813
EB <byrumeb@gmail.com>
2020-11-21 02:12:12 -0500

    nav issues

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,3 @@
-<div class="topnav">
-  <a class="active"   href="#index.html">Home</a>
-  <a href="#inside-page-one">News</a>
-  <a href="#inside-page-two">Gallery</a>
-  <a href="#inside-page-three">About</a>
-</div>
-
-
 /* Navbar */
 
 
